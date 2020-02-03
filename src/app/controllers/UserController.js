f2b2da7545992831597c58521d21eb0b78fb6bc0
Guilherme Jeronimo <gjpnkflyd@gmail.com>
2020-02-03 16:27:00 -0300

    listando agendamentos do user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,7 @@
 import * as Yup from 'yup';
 import User from '../models/User';
 
-class USercontroller {
+class UserController {
   async store(req, res) {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
@@ -81,4 +81,4 @@ class USercontroller {
   }
 }
 
-export default new USercontroller();
+export default new UserController();
