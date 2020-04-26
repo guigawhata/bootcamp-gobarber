c45704d931527fac7c1571d93ac7bf82508aba2f
Guilherme Jeronimo <gjpnkflyd@gmail.com>
2020-04-26 10:04:33 -0300

    init web, backend fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import 'express-async-errors';
 import path from 'path';
+import cors from 'cors';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import routes from './routes';
@@ -22,6 +23,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -36,7 +38,7 @@ class App {
 
   exceptionHandler() {
     this.server.use(async (err, req, res, next) => {
-      if (process.env.NODE_ENV == 'development') {
+      if (process.env.NODE_ENV === 'development') {
         const errors = await new Youch(err, req).toJSON();
 
         return res.status(500).json(errors);
