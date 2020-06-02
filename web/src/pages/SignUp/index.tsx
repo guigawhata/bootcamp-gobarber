a1c7a56b48c77c0e8079966fe91b03ab17ad8e18
Guilherme Jeronimo <gjpnkflyd@gmail.com>
2020-06-02 18:25:03 -0300

    UNFORM, USABILITY, COMPONENTS

---
+++ web/src/pages/SignUp/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
+
+import { Container, Content, Background } from './styles';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logo from '../../assets/logo.svg';
+
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logo} alt="Go Barber" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+          <Input
+            name="name"
+            type="text"
+            placeholder="Nome Completo"
+            icon={FiUser}
+          />
+          <Input name="email" type="text" placeholder="E-mail" icon={FiMail} />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Senha"
+            icon={FiLock}
+          />
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="create">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
